3cab512d7505503329fac6e680c37bed521a2c6f
Aditi <aditipaikrao2799@gmail.com>
2022-02-13 17:25:16 +0530

    done

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Petshop</a> 
+         {/* '/'is used as source file i.e. if we click on it we will gone to the source file here source is this only   */}
+      </header>
+      <main>list product</main>
+
+    </div>
+  );
+}
+
+export default App;
